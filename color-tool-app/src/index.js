7579fb0c97e374690722a7adc687dc082ad50be7
Eric Greene <eric@t4d.io>
2019-12-09 17:04:26 -0500

    add code demos

--- color-tool-app/src/index.js
+++ color-tool-app/src/index.js
@@ -1,58 +1,28 @@
 require('angular');
 
-// class P { }
-// typeof P => function
+import toolHeaderTpl from './templates/tool-header.html';
+import colorToolTpl from './templates/color-tool.html';
 
 angular.module('ColorToolApp', [])
   .directive('toolHeader', function() {
 
     return {
       restrict: 'E', // selector: 'tool-header',
-      // templateUrl: '/tool-header.html', // templateUrl
-      template: `
-        <header>
-          <h1>{{vm.headerText}}</h1>
-        </header>
-      `,
-      // Modern Angular is the Component Class
+      template: toolHeaderTpl,
       controllerAs: 'vm', // sets the vm in the template
       bindToController: true,
       scope: {
         headerText: '@',
       },
-      controller: function($scope) {
-        // const vm = this;
-        // bindToController allows me to skip this
-        // assignment step
-        // vm.headerText = $scope.headerText;
+      controller: function() {
       },
     };
 
   })
-  // Angular Component
   .directive('colorTool', function() {
     return {
       restrict: 'E',
-      // templateUrl: '/color-tool.html',
-      template: `
-        <tool-header header-text="Color Tool!!"></tool-header>
-
-        <ul>
-          <li ng-repeat="color in vm.colors">{{color}}</li>
-        </ul>
-
-        <form novalidate>
-
-          <div>
-            <label for="color-input">Color:</label>
-            <input type="text" id="color-input" name="color"
-              ng-model="vm.colorForm.color" />
-          </div>
-
-          <button type="button" ng-click="vm.addColor()">Add Color</button>
-
-        </form>            
-      `,
+      template: colorToolTpl,
       controllerAs: 'vm',
       scope: {},
       controller: function() {
