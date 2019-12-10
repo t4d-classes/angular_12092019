8a369bb999385df5ad77f2a12a5ba0c294670466
Eric Greene <eric@t4d.io>
2019-12-10 17:11:44 -0500

    add code demos

---
+++ demo-app/src/app/color-tool/components/color-home/color-home.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+
+@Component({
+  selector: 'app-color-home',
+  templateUrl: './color-home.component.html',
+  styleUrls: ['./color-home.component.css']
+})
+export class ColorHomeComponent implements OnInit {
+
+  headerText = 'Color Tool';
+
+  colors = [ 'red', 'green', 'blue' ];
+
+  colorForm: FormGroup;
+
+  constructor(private fb: FormBuilder) { }
+
+  ngOnInit() {
+
+    this.colorForm = this.fb.group({
+      color: '',
+    });
+
+  }
+
+  addColor() {
+
+    this.colors = this.colors
+      .concat(this.colorForm.value.color);
+
+  }
+
+}
---
+++ exercises/exercise-11.md
@@ -0,0 +1,16 @@
+# Exercise 11
+
+1. Copy the array of car objects from the AngularJS version of the applicaton. Each object should have the following properties and types for those properties.
+
+id: number
+make: string
+model: string
+year: number
+color: string
+price: number
+
+Do not use an interface nor a class. Create the array in car home component.
+
+2. Display a table of cars using the cars array. Be sure to include column headers and use the appropriate semantic tags for table rows.
+
+3. Run the application and verify it works. Be sure to check your terminal window and browser console for errors.
