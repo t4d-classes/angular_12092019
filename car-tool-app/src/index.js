cafff0e13de46307fa66c0f8ad2ba88071bd13a9
Eric Greene <eric@t4d.io>
2019-12-09 19:07:59 -0500

    add code demos

--- car-tool-app/src/index.js
+++ car-tool-app/src/index.js
@@ -1,72 +1,111 @@
 require('angular');
 
+import toolHeaderTpl from './templates/tool-header.html';
+import carTableTpl from './templates/car-table.html';
+import carToolTpl from './templates/car-tool.html';
+
 angular.module('CarToolApp', [])
-  .controller('CarToolController',
-    function($scope) {
+  .directive('toolHeader', function() {
 
-      $scope.headerText = 'Car Tool!';
+    return {
+      restrict: 'E',
+      template: toolHeaderTpl,
+      scope: {
+        headerText: '@',
+      },
+      bindToController: true,
+      controllerAs: 'vm',
+      controller: function() { },
+    };
 
-      $scope.cars = [
-        { id: 1, make: 'Ford', model: 'Fusion Hybrid', year: 2018, color: 'white', price: 45000 },
-        { id: 2, make: 'Tesla', model: 'S', year: 2019, color: 'blue', price: 100000 },
-      ];
+  })
+  .directive('carTable', function() {
 
-      $scope.editCarId = -1;
+    return {
+      restrict: 'E',
+      template: carTableTpl,
+      scope: {
+        cars: '=',
+        editCarId: '=',
+      },
+      bindToController: true,
+      controllerAs: 'vm',
+      controller: function() { },
+    };
 
-      $scope.carForm = {
-        make: '',
-        model: '',
-        year: 1900,
-        color: '',
-        price: 0,
-      };
+  })
+  .directive('carTool', function() {
 
-      // $scope.editCarForm = {
-      //   make: $scope.cars[1].make,
-      // };
+    return {
+      restrict: 'E',
+      template: carToolTpl,
+      scope: { },
+      bindToController: true,
+      controllerAs: 'vm',
+      controller: function() {
 
-      $scope.addCar = function(carForm) {
+        const vm = this;
 
-        const car = {
-          ...carForm,
-          id: Math.max(...$scope.cars.map(c => c.id), 0) + 1,
+        vm.cars = [
+          { id: 1, make: 'Ford', model: 'Fusion Hybrid', year: 2018, color: 'white', price: 45000 },
+          { id: 2, make: 'Tesla', model: 'S', year: 2019, color: 'blue', price: 100000 },
+        ];
+  
+        vm.editCarId = -1;
+  
+        vm.carForm = {
+          make: '',
+          model: '',
+          year: 1900,
+          color: '',
+          price: 0,
         };
-
-        $scope.cars = $scope.cars.concat(car);
-
-      };
-
-      $scope.editCar = function(carId) {
-
-        const carIndex = $scope.cars.findIndex(c => c.id === carId);
-
-        $scope.editCarForm = {
-          ...$scope.cars[carIndex],
+  
+        vm.addCar = function(carForm) {
+  
+          const car = {
+            ...carForm,
+            id: Math.max(...vm.cars.map(c => c.id), 0) + 1,
+          };
+  
+          vm.cars = vm.cars.concat(car);
+  
         };
-
-        $scope.editCarId = carId;
-      }
-
-      $scope.deleteCar = function(carId) {
-        $scope.cars = $scope.cars.filter(c => c.id !== carId);
-      };
-
-      $scope.saveCar = function(carId, editCarForm) {
-        const car = {
-          ...editCarForm,
-          id: carId,
+  
+        vm.editCar = function(carId) {
+  
+          const carIndex = vm.cars.findIndex(c => c.id === carId);
+  
+          vm.editCarForm = {
+            ...vm.cars[carIndex],
+          };
+  
+          vm.editCarId = carId;
+        }
+  
+        vm.deleteCar = function(carId) {
+          vm.cars = vm.cars.filter(c => c.id !== carId);
         };
+  
+        vm.saveCar = function(carId, editCarForm) {
+          const car = {
+            ...editCarForm,
+            id: carId,
+          };
+  
+          const carIndex = vm.cars.findIndex(c => c.id === carId);
+  
+          vm.cars = vm.cars.concat();
+          vm.cars[carIndex] = car;
+          vm.editCarId = -1;
+        };
+  
+        vm.cancelCar = function() {
+          vm.editCarId = -1;
+        };        
 
-        const carIndex = $scope.cars.findIndex(c => c.id === carId);
+      },
+    };
 
-        $scope.cars = $scope.cars.concat();
-        $scope.cars[carIndex] = car;
-        $scope.editCarId = -1;
-      };
-
-      $scope.cancelCar = function() {
-        $scope.editCarId = -1;
-      };
-
-    });
+  });
 
--- exercises/exercise-7.md
+++ exercises/exercise-7.md
@@ -4,7 +4,7 @@
 
 1. Utilize the `toolHeader` directive created during the demo in the Color Tool App. Please just copy and paste the relevant parts of the code into the Car Tool App. Replace the current header content with the new directive.
 
-2. Move the car table element and its children to a new directive named `carTable`. Do not include support for the delete button (this will be done later).
+2. Move the car table element and its children to a new directive named `carTable`. Do not include support for the delete, edit, save and cancel buttons (this will be done later).
 
 3. Replace the original car table with the new `carTable` directive.
 
